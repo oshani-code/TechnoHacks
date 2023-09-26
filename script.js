08ffe6112c694f6729d1e150c2a317b4ccef465c
Oshani Sharma <85727177+oshani-code@users.noreply.github.com>
2023-09-26 22:20:00 +0530

    Currency Converter

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-// Set the date and time for the countdown
-const countdownDate = new Date("2024-05-31 23:59:59").getTime();
 
-// Update the countdown every 1 second
-const countdownInterval = setInterval(function() {
-    const now = new Date().getTime();
-    const timeLeft = countdownDate - now;
+var select = document.querySelectorAll('.currency');
+input_currency = document.getElementById('input_currency');
+output_currency = document.getElementById('output_currency');
 
-    if (timeLeft <= 0) {
-        clearInterval(countdownInterval);
-        document.getElementById("countdown").innerHTML = "Countdown expired!";
-    } else {
-        const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-                const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-                const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+const host = 'api.frankfurter.app';
+fetch(`https://${host}/currencies`)
+.then(data => data.json())
+.then((data) => {
+    const entries = Object.entries(data)
+    for(i=0; i<entries.length; i++){
+        select[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}"</option>`
+        select[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}"</option>`
+    }
+});
 
-                document.getElementById("countdown").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-            }
-        }, 1000); // 1000 milliseconds = 1 second
+function converter(){
+    var input_currency_val = input_currency.value;
+    if(select[0].value != select[1].value){
+        const host = 'api.frankfurter.app';
+        fetch(`https://${host}/latest?amount=${input_currency_val}&from=${select[0].value}&to=${select[1].value}`)
+        .then(val => val.json())
+        .then((val) => {
+            output_currency.value = Object.values(val.rates)[0]
+        });
+    }
+
+    else{
+        alert("Please select two different currencies");
+    }
+}
